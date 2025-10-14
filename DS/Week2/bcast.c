8b57d4eb6dce4edfb225095837b226c35420b1eb
Colorfast <sc23jdm@leeds.ac.uk>
2025-10-14 17:50:17 +0100

    week 2

--- DS/Week2/bcast.c
+++ DS/Week2/bcast.c
@@ -10,20 +10,41 @@ int main( argc, argv )
 int argc;
 char **argv;
 {
-    int rank, temp;
+    int rank, size, temp, value;
     MPI_Init( &argc, &argv );
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    do {
-	if (rank == 0)
-	{ 
-            printf("Input a number: ");
-	    scanf( "%d", &temp );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+    while (1) {
+        if (rank == 0) {
+            printf("Input a number (negative to exit): ");
+            scanf("%d", &value);
         }
-	MPI_Bcast( &temp, 1, MPI_INT, 0, MPI_COMM_WORLD );
-	
-	printf( "Process %d got %d\n", rank, temp );
-    } while (temp >= 0);
+
+        // Broadcast the value from process 0 to all other processes.
+        // This also serves to inform other processes if they should exit.
+        MPI_Bcast(&value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // If the value is negative, all processes break out of the loop.
+        if (value < 0) {
+            break;
+        }
+
+        printf("Process %d received the broadcast value %d\n", rank, value);
+
+        int sum = 0;
+        // Each process contributes the 'value' it received.
+        // The results are summed and stored in 'sum' on process 0.
+        MPI_Reduce(&value, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        // Only the root process (rank 0) prints the final sum for this iteration.
+        if (rank == 0) {
+            printf("Process 0 calculated the sum to be: %d\n\n", sum);
+        }
+    }
+
+    printf("Process %d is finalizing.\n", rank);
 
     MPI_Finalize( );
     return 0;
--- DS/Week2/ring.c
+++ DS/Week2/ring.c
@@ -17,25 +17,30 @@ char **argv;
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
+    
 
    char         processorname[100];
    int          namelen;
-
    MPI_Get_processor_name( processorname, &namelen );
-	
-        if (rank == 0) {
+    if (rank == 0) {
 	    printf("Input a value : ");
-            scanf( "%d", &value );
+        scanf( "%d", &value );
 	    MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
+		/* Now, receive from the last process */
+		MPI_Recv( &value, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &status );
 	}
 	else {
 	    MPI_Recv( &value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, 
 		      &status );
-            value++;
+        value++;
 	    if (rank < size - 1)
-            {
+        {
 		MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
-            }
+        }
+		else /* Last process, send back to rank 0 */
+		{
+			MPI_Send( &value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD );
+		}
 	}
 	printf( "Process %d on %s got %d\n", rank, processorname, value );
 
