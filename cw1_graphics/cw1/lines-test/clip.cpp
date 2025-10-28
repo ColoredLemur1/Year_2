a2385fbec7ad7b159345367972c7ee534be33e82
ColoredLemur1 <sc23jdm@leeds.ac.uk>
2025-10-28 21:11:00 +0000

    Finished lines tests

--- cw1_graphics/cw1/lines-test/clip.cpp
+++ cw1_graphics/cw1/lines-test/clip.cpp
@@ -8,164 +8,19 @@
 #include "../draw2d/draw.hpp"
 
 
-TEST_CASE( "Partially offscreen", "[clip]" )
-{
-	Surface surface( 640, 480 );
-	surface.clear();
+TEST_CASE("Partially offscreen", "[clip]"){
+    Surface surface( 640, 480 );
+    surface.clear();
 
-	// Test all inside/outside combinations 
-	
-	SECTION( "inside to right" )
-	{
-		draw_line_solid( surface,
-			{ 100.f, 240.f },
-			{ 1920.f, 240.f },
-			{ 255, 255, 255 }
-		);
+    //Only one test case for now
+    SECTION( "go right" ){
+        draw_line_solid( surface,
+            { 1.f, 240.f },
+            { 1920.f, 240.f },
+            { 255, 255, 255 }
+        );
 
 		auto const pixels = max_col_pixel_count( surface );
 		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "left to inside" )
-	{
-		draw_line_solid( surface,
-			{ -100.f, 240.f },
-			{ 100.f, 240.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_col_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "inside to left" )
-	{
-		draw_line_solid( surface,
-			{ 100.f, 240.f },
-			{ -100.f, 240.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_col_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "right to inside" )
-	{
-		draw_line_solid( surface,
-			{ 1920.f, 240.f },
-			{ 100.f, 240.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_col_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "inside to top" )
-	{
-		draw_line_solid( surface,
-			{ 320.f, 100.f },
-			{ 320.f, -100.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "bottom to inside" )
-	{
-		draw_line_solid( surface,
-			{ 320.f, 600.f },
-			{ 320.f, 100.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "inside to bottom" )
-	{
-		draw_line_solid( surface,
-			{ 320.f, 100.f },
-			{ 320.f, 600.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "top to inside" )
-	{
-		draw_line_solid( surface,
-			{ 320.f, -100.f },
-			{ 320.f, 100.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "diagonal inside to outside" )
-	{
-		draw_line_solid( surface,
-			{ 100.f, 100.f },
-			{ 800.f, 600.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "diagonal outside to inside" )
-	{
-		draw_line_solid( surface,
-			{ -100.f, -100.f },
-			{ 100.f, 100.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "crossing horizontally" )
-	{
-		draw_line_solid( surface,
-			{ -100.f, 240.f },
-			{ 800.f, 240.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_col_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
-	
-	SECTION( "crossing vertically" )
-	{
-		draw_line_solid( surface,
-			{ 320.f, -100.f },
-			{ 320.f, 600.f },
-			{ 255, 255, 255 }
-		);
-
-		auto const pixels = max_row_pixel_count( surface );
-		REQUIRE( 1 == pixels );
-		REQUIRE( pixels > 0 ); 
-	}
+    }
 }
--- cw1_graphics/cw1/lines-test/scenarios.cpp
+++ cw1_graphics/cw1/lines-test/scenarios.cpp
@@ -1,3 +1,319 @@
 #include <catch2/catch_amalgamated.hpp>
 
-//TODO
+#include <algorithm>
+
+#include "helpers.hpp"
+
+#include "../draw2d/surface.hpp"
+#include "../draw2d/draw.hpp"
+TEST_CASE( "Partially offscreen", "[scenarios]" ){
+	Surface surface( 100, 100 );
+	surface.clear();
+
+	// Test all inside/outside combinations 
+	
+	SECTION( "inside to right" )
+	{
+		draw_line_solid( surface,
+			{ 10.f, 50.f },
+			{ 200.f, 50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_col_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "left to inside" )
+	{
+		draw_line_solid( surface,
+			{ -50.f, 50.f },
+			{ 10.f, 50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_col_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "inside to left" )
+	{
+		draw_line_solid( surface,
+			{ 10.f, 50.f },
+			{ -50.f, 50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_col_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "right to inside" )
+	{
+		draw_line_solid( surface,
+			{ 200.f, 50.f },
+			{ 10.f, 50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_col_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "inside to top" )
+	{
+		draw_line_solid( surface,
+			{ 50.f, 10.f },
+			{ 50.f, -50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "bottom to inside" )
+	{
+		draw_line_solid( surface,
+			{ 50.f, 150.f },
+			{ 50.f, 10.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "inside to bottom" )
+	{
+		draw_line_solid( surface,
+			{ 50.f, 10.f },
+			{ 50.f, 150.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "top to inside" )
+	{
+		draw_line_solid( surface,
+			{ 50.f, -50.f },
+			{ 50.f, 10.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "diagonal inside to outside" )
+	{
+		draw_line_solid( surface,
+			{ 10.f, 10.f },
+			{ 150.f, 150.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "diagonal outside to inside" )
+	{
+		draw_line_solid( surface,
+			{ -50.f, -50.f },
+			{ 10.f, 10.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "crossing horizontally" )
+	{
+		draw_line_solid( surface,
+			{ -50.f, 50.f },
+			{ 150.f, 50.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_col_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+	
+	SECTION( "crossing vertically" )
+	{
+		draw_line_solid( surface,
+			{ 50.f, -50.f },
+			{ 50.f, 150.f },
+			{ 255, 255, 255 }
+		);
+
+		auto const pixels = max_row_pixel_count( surface );
+		REQUIRE( 1 == pixels );
+		REQUIRE( pixels > 0 ); 
+	}
+}
+TEST_CASE( "Thin lines", "[scenarios]" ){
+    Surface surface( 100, 100 );
+    surface.clear();
+
+    //test all the thin line cases
+    SECTION( "horizontal" ){
+        draw_line_solid( surface,
+            { 10.f, 50.f },
+            { 90.f, 50.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( max_row_pixel_count( surface ) > 1 );
+        REQUIRE( 1 == max_col_pixel_count( surface ) );
+    }
+
+    SECTION( "vertical" ){
+        draw_line_solid( surface,
+            { 50.f, 10.f },
+            { 50.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( 1 == max_row_pixel_count( surface ) );
+        REQUIRE( max_col_pixel_count( surface ) > 1 );
+    }
+    //diagonal line that chnages more in the x direction than the y direction
+    SECTION( "x-major" ){
+        draw_line_solid( surface,
+            { 10.f, 20.f },
+            { 90.f, 40.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( max_row_pixel_count( surface ) > 1 );
+        REQUIRE( 1 == max_col_pixel_count( surface ) );
+    }
+    //diagonal line that chnages more in the y direction than the x direction
+    SECTION( "y-major" ){
+        draw_line_solid( surface,
+            { 50.f, 10.f },
+            { 40.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( 1 == max_row_pixel_count( surface ) );
+        REQUIRE( max_col_pixel_count( surface ) > 1 );
+    }
+    //diagonal line that changes equally in the x and y directions
+    SECTION( "diagonal" ){
+        draw_line_solid( surface,
+            { 10.f, 10.f },
+            { 90.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( 1 == max_row_pixel_count( surface ) );
+        REQUIRE( 1 == max_col_pixel_count( surface ) );
+    }
+}
+TEST_CASE( "No gaps", "[scenarios]" ){
+    Surface surface( 100, 100 );
+    surface.clear();
+
+    //test all the no gaps cases
+    SECTION( "horizontal" ){
+        draw_line_solid( surface,
+            { 10.f, 50.f },
+            { 90.f, 50.f },
+            { 255, 255, 255 }
+        );
+
+        REQUIRE( max_row_pixel_count( surface ) > 1 );
+        REQUIRE( 1 == max_col_pixel_count( surface ) );
+    }
+    SECTION( "vertical" ){
+        draw_line_solid( surface,
+            { 50.f, 10.f },
+            { 50.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        auto const counts = count_pixel_neighbours( surface );
+        REQUIRE( 2 == counts[1] );
+        REQUIRE( counts[2] > 0 );
+        REQUIRE( 0 == counts[0] );
+        for( std::size_t i = 3; i < counts.size(); ++i )
+            REQUIRE( 0 == counts[i]  );
+    }
+    SECTION( "diagonal" ){
+        draw_line_solid( surface,
+            { 10.f, 10.f },
+            { 90.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        auto const counts = count_pixel_neighbours( surface );
+        REQUIRE( 2 == counts[1] );
+        REQUIRE( counts[2] > 0 );
+        REQUIRE( 0 == counts[0] );
+        for( std::size_t i = 3; i < counts.size(); ++i )
+            REQUIRE( 0 == counts[i]  );
+    }
+    SECTION( "slanted high" ){
+        draw_line_solid( surface,
+            { 10.f, 10.f },
+            { 10.f, 90.f },
+            { 255, 255, 255 }
+        );
+
+        auto const counts = count_pixel_neighbours( surface );
+        REQUIRE( 2 == counts[1] );
+        REQUIRE( counts[2] > 0 );
+        REQUIRE( 0 == counts[0] );
+        for( std::size_t i = 3; i < counts.size(); ++i )
+            REQUIRE( 0 == counts[i]  );
+    }
+    SECTION( "slanted low" ){
+        draw_line_solid( surface,
+            { 10.f, 90.f },
+            { 10.f, 10.f },
+            { 255, 255, 255 }
+        );
+
+        auto const counts = count_pixel_neighbours( surface );
+        REQUIRE( 2 == counts[1] );
+        REQUIRE( counts[2] > 0 );
+        REQUIRE( 0 == counts[0] );
+        for( std::size_t i = 3; i < counts.size(); ++i )
+            REQUIRE( 0 == counts[i]  );
+    }
+}
+
+TEST_CASE( "Special cases", "[scenarios]" ){
+    Surface surface( 100, 100 );
+    surface.clear();
+
+    //test all the special cases
+    SECTION( "zero length" ){
+        draw_line_solid( surface,
+            { 10.f, 10.f },
+            { 10.f, 10.f },
+            { 255, 255, 255 }
+        );
+
+        auto const counts = count_pixel_neighbours( surface );
+        REQUIRE( 0 == counts[0] );
+        for( std::size_t i = 1; i < counts.size(); ++i )
+            REQUIRE( 0 == counts[i]  );
+    }
+}
